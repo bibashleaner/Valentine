b7404e1f0d2260263f37e64da18fbbffd82b588f
bibashlearner <bibashchaudhary330@gmail.com>
2024-08-05 21:22:03 +0545

    completed

--- main.js
+++ main.js
@@ -1,34 +1,18 @@
 const yesBtn = document.querySelector('#A');
 const noBtn = document.querySelector('#B');
 
-// let yesHeight = 50;
-// let yesWidht = 100;
-// let yesFont = 25;
+let yesHeight = 50;
+let yesWidth = 100;
+let yesFont = 25;
 
-// if(noBtn){
-//     noBtn.addEventListener('click', function(){
-//         yesHeight = yesHeight + 50;
-//         yesWidth = yesWidht = 50;
-//         yesFont += 10;
-//         yesBtn.style.height = `${yesHeight}px`;
-//         yesBtn.style.width = `${yesWidht}px`;
-//         yesBtn.style.fontSize = `${yesFont}px`;
-//     });
-// }
-        
-// noBtn.addEventListener('mouseover', clickNoBtn);
-
-// function clickNoBtn(){
-//     const i = Math.floor(Math.random() * 500)+1;
-//     const j = Math.floor(Math.random() * 500)+1;
-
-//     noBtn.style.left =  i+'px';
-//     noBtn.style.top = j+'px';
-// }
-
-if(onclick="noBtn"){
-    noBtn.addEventListener('click', ()=>{
-        // const currentHeight = yesBtn.getBoundingClientRect();
-        yesBtn.style.height = yesBtn + 100 + "px";
+if(noBtn){
+    noBtn.addEventListener('click', function(){
+        yesHeight = yesHeight + 50;
+        yesWidth = yesWidth + 50;
+        yesFont += 10;
+        yesBtn.style.height = `${yesHeight}px`;
+        yesBtn.style.width = `${yesWidth}px`;
+        yesBtn.style.fontSize = `${yesFont}px`;
     });
 }
+        
